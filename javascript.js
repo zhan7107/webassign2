0570adb1006e9d715524549b0d6deb641d187714
Hongli Zhang <43276921+zhan7107@users.noreply.github.com>
2018-11-07 14:20:29 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -521,27 +521,27 @@ window.onload = function () {
 					parameter="&parameter="+this.particle;
 					hm.style.display='block';
 				}else{
-					document.getElementById("legendImg").src = "Capture.png";
+					document.getElementById("legendImg").src = "legend/Capture.PNG";
 					hm.style.display='none';
 				}
 				
 				if(this.particle === 'pm25'){
-					document.getElementById("legendImg").src = "pm25.png";
+					document.getElementById("legendImg").src = "legend/pm25.PNG";
 				}
 				else if (this.particle === 'pm10'){
-					document.getElementById("legendImg").src = "pm10.png";
+					document.getElementById("legendImg").src = "legend/pm10.PNG";
 				}
 				else if (this.particle === 'so2'){
-					document.getElementById("legendImg").src = "so2.png";
+					document.getElementById("legendImg").src = "legend/so2.PNG";
 				}
 				else if (this.particle === 'no2'){
-					document.getElementById("legendImg").src = "no2.png";
+					document.getElementById("legendImg").src = "legend/no2.PNG";
 				}
 				else if (this.particle === 'o3'){
-					document.getElementById("legendImg").src = "o3.png";				
+					document.getElementById("legendImg").src = "legend/o3.PNG";				
 				}
 				else if (this.particle === 'co'){
-					document.getElementById("legendImg").src = "co.png";
+					document.getElementById("legendImg").src = "legend/co.PNG";
 				}
 				
 				if(tableModes.mode == 'latest') {
